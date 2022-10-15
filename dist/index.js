ae35531287aea59c5374eb7774202a4984651e7e
Cole Peaden <dcpeaden@gmail.com>
2022-10-15 08:10:01 -0500

    added gitignore (#4)

--- dist/index.js
+++ dist/index.js
@@ -3,22 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
 const core_1 = require("@mikro-orm/core");
-const mikro_orm_config_1 = __importDefault(require("./src/mikro-orm.config"));
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const type_graphql_1 = require("type-graphql");
-const hello_1 = require("./src/resolvers/hello");
+const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
-    const app = (0, express_1.default)();
     const apollo = new apollo_server_express_1.ApolloServer({
         schema: await (0, type_graphql_1.buildSchema)({
-            resolvers: [hello_1.HelloResolver],
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver],
             validate: false,
         }),
+        context: () => ({ em: orm.em }),
     });
+    const app = (0, express_1.default)();
     await apollo.start();
     apollo.applyMiddleware({ app });
     app.listen(4000, () => {
