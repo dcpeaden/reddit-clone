038e1970fe032ab943608f044b4881a8ab405bba
Cole Peaden <dcpeaden@gmail.com>
2022-10-15 08:06:43 -0500

    added Post and User and cleanup (#3)

---
+++ dist/mikro-orm.config.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const constants_1 = require("./constants");
+const path_1 = __importDefault(require("path"));
+const Post_1 = require("./entities/Post");
+exports.default = {
+    migrations: {
+        path: path_1.default.join(__dirname, "./migrations"),
+        glob: "!(*.d).{js,ts}",
+    },
+    allowGlobalContext: true,
+    entities: [Post_1.Post],
+    dbName: "reddit-clone",
+    type: "postgresql",
+    debug: !constants_1.__prod__,
+};
+//# sourceMappingURL=mikro-orm.config.js.map
